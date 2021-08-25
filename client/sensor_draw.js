7b655ca5bb1b03d2fa2063f81ec8b6fd52f4584a
Derek_Tang <derek_tang@tw.shuttle.com>
2021-08-25 19:40:07 +0800

    Disable visual sensor color

--- client/sensor_draw.js
+++ client/sensor_draw.js
@@ -56,6 +56,7 @@ function remap(sensor) {
 			}
 		}
 	}
+	/*
 	for (i=1;i<10;i++) { // create visual sensor
 		for(j=0;j<5;j++) {
 			if (sensor_mux[i][j]===0) { // up and flat
@@ -87,7 +88,7 @@ function remap(sensor) {
 				cnt=0;
 			}
 		}
-	}
+	} */
 }
 
 
