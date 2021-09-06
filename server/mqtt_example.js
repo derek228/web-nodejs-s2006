227cc6c65b83319ab78fd6390e894a3c26ea7e26
Derek_Tang <derek_tang@tw.shuttle.com>
2021-09-06 14:27:35 +0800

    Fix duplicate log message when browser create a now connection

--- server/mqtt_example.js
+++ server/mqtt_example.js
@@ -1,10 +1,71 @@
+var mqtt = require('mqtt');
+module.exports=function() {
+	client=null;
+	//deviceMac=null;
+	this.init=function(ip,mac,cb) {
+		//deviceMac=mac;
+		console.log("New MQTT IP : ", ip)
+		if (client != null) {
+			console.log("MQTT Init: ", client.options.host);
+			if (ip == client.options.host) {
+				console.log("Already connect to MQTT : ", ip);
+				return;
+			}
+			if (client!=null) {
+				console.log("Close MQTT:"+client.options.host+"..... Start Connect to MQTT :"+ip);
+				client=client.end(true);
+			}
+		}
+		let opt={
+			port:1883,
+			clientID: mac,
+			username:'ZDlhN2E4MjgwNTY4',
+			password:'NDg5YWQyYTAyYTg3ZGIyZGExMTgzNTc0'
+		}
+		if (ip) {
+			ip = "mqtt://"+ip;
+			console.log("MQTT Init IP : ", ip);
+			client = mqtt.connect(ip,opt);
+		}
+		else {
+			console.log("MQTT server in localhost");
+			client = mqtt.connect('mqtt://localhost', opt);
+		}
+		console.log("MQTT initialize done...");
+
+		client.on('connect', function() {
+			console.log("MQTT connected...:" , client);
+			client.subscribe('S2006/DeviceReportInformation');
+		});
+		
+		client.on('close',function() {
+			if (client) {
+				console.log("Force close mqtt: ", client.nexdID);
+			}
+			else {
+				console.log("MQTT Client is NULL");
+			}
+		  });
+		  client.on('message', function(topic, msg,pkg) {
+			  console.log("mqtt client: ", client.options.clientID);
+			  //console.log("Force close mqtt: ", client.nextId);
+			  //console.log("Package: ", pkg);
+			  var d = Date.now();
+			  var dd = new Date(d);
+			  var dateStr=dd.getHours()+'_'+dd.getMinutes()+'_'+dd.getSeconds();
+			  console.log(dateStr);
+			  cb(msg);
+			});
+	}
+}
+/*
 const s2006_mqtt = {};
 var AESCrypt = require('./aes_crypto.js')
 var mqtt = require('mqtt');
 var client = null;
 var deviceMAC=null;
 var addr=null;
-s2006_mqtt.init=function(ip) {
+s2006_mqtt.init=function(ip, cb ) {
 	
 	var opt = { 
 	port:1883,
@@ -15,8 +76,9 @@ s2006_mqtt.init=function(ip) {
 	
 	console.log("mqtt intt ip :"+ip);
 	if (ip) {
-		console.log("MQTT Server Connect to =="+ip);
+		console.log("MQTT Server Connect to == "+ip);
 		client = mqtt.connect(ip, opt);
+		s2006_mqtt.client=client;
 		addr = Buffer.from(ip);
 	}
 	else {
@@ -38,11 +100,35 @@ s2006_mqtt.init=function(ip) {
 		}
 		//client=s2006_mqtt.init(addr);
 	  });
+	  client.on('message', function(topic, msg) {
+		  console.log("mqtt: ", msg);
+		  cb(msg);
+		/*data = AESCrypt.decrypt(msg);
+		  json=JSON.parse(data);
+		  json.time=util.getTime();
+		  //console.log(json);
+		  if (filterMAC) {
+			if  (json.mac_address == filterMAC){
+			  sf.parser(json);
+			  console.log('======================= Get Sensor Data From MQTT '+filterMAC+'=========================' + json.time);
+			  socket.emit('message', {'message': data});
+			  log.write(JSON.stringify(json),log.filename);
+			  //log_to_file(JSON.stringify(json));
+			}
+			else {
+			  console.log('Device MAC:' +json.mac_address+ ' ( != )  ' +'Filter MAC'+  filterMAC+ '  == Data Length ==  ' + data.length);
+			}
+		  }
+		  else {
+			console.log("No define S2006 MAC==="+data );
+		  }
+		  //socket.emit('message', {'message': data});
+		});
 	
 	
 	//return client;
 }
-
+*/
 /*
 s2006_mqtt.init=function(ip) {
 	
@@ -56,12 +142,14 @@ s2006_mqtt.init=function(ip) {
 	console.log('s2006 mqtt initialized(ip): '+ip);
 	return client;
 }
-*/
+
 s2006_mqtt.setMAC=function(mac) {
 	console.log('Set Device MAC : '+mac);
 	deviceMAC=Buffer.from(mac);
 }
-s2006_mqtt.client = function() {
+//s2006_mqtt.client = client;
+/*s2006_mqtt.client = function() {
 	return client;
 }
-module.exports = s2006_mqtt;
+*/
+//module.exports = s2006_mqtt;
--- server/server.js
+++ server/server.js
@@ -3,12 +3,14 @@ const delay=require('delay');
 var http = require("http");
 var url = require('url');
 var fs = require('fs');
-var s2006_mqtt = require('./mqtt_example.js');
+//var s2006_mqtt = require('./mqtt_example.js');
+var mqtt_example = require('./mqtt_example.js');
 var AESCrypt = require('./aes_crypto.js');
 var util=require('./s2006_utility');
 var lg=require('./log.js');
 var log=new lg();
 var sf=require('./sensor_parser.js');
+var s2006_mqtt=new mqtt_example();
 // global variable
 var client=null;
 var addr=null;
@@ -17,51 +19,27 @@ var file_title='';
 var custom_port=8001;
 var server_log_en=1;
 var ftpHost='';
-/*
-var net = require('net');
-var socket = net.createConnection(80, 'www.google.com');
-socket.on('connect', function() {
-  console.log(socket.address().address);
-  socket.end();
-});
-*/
-//function custom_port_parser() {
-	if (process.argv.length>2) {
-		custom_port=Number(process.argv[2]);
-		console.log(typeof custom_port, custom_port);
-	}
-	else
-	{
-		console.log("Use Default Port "+custom_port);
-	}	
-//}
+if (process.argv.length>2) {
+  custom_port=Number(process.argv[2]);
+	console.log(typeof custom_port, custom_port);
+}
+else
+{
+	console.log("Use Default Port "+custom_port);
+}	
 
+  
 var server = http.createServer(function(request, response) {
   //console.log("request.url: "+request.url);
   var hostname='http://localhost:'+custom_port+request.url;
   var myURL= new URL(hostname);
-  //console.log(myURL+" === "+hostname);
-  //console.log("host: "+myURL.host);
-  //console.log("hostname: "+myURL.hostname);
-  //console.log("href:"+myURL.href);
-  //var myURL= new URL("http://localhost:8003"+request.url);
   var path =myURL.pathname;
-  //console.log("REQUEST:"+request.url);
-  //console.log("DIR+PATH:"+__dirname + path);
   var ftpPath=__dirname+'/..'+path;
-  //console.log("FTP : "+ftpPath);
   switch (path) {
     case '/':
-      //console.log("root"+ftpPath);
-
-      //console.log(path);
       if (ftpHost!=='') {
       util.listFiles(ftpPath, response,ftpHost) ;
       }
-      //util.listFiles(ftpPath, response,"http://localshot:8001") ;
-      //response.writeHead(200, {'Content-Type': 'text/html'});
-      //response.write('Hello, World.');
-      //response.end();
       break;
       case '/favicon.ico':
       response.writeHead(200, {'Content-Type': 'text/html'});
@@ -75,16 +53,19 @@ var server = http.createServer(function(request, response) {
          mac=myURL.searchParams.get('mac');
          file_title=myURL.searchParams.get('file');
          if (addr) {
-          //if (util.checkIP(addr)==true){
-            addr = "mqtt://"+addr;
-            s2006_mqtt.client().end  ();
+            //addr = "mqtt://"+addr;
+            if (s2006_mqtt.client  != null) {
+              console.log("MQTT client close "+ s2006_mqtt.client);
+            }
             console.log("Connect To New MQTT IP: "+addr);
             client = null;
-            client=s2006_mqtt.init(addr);
-           //}
-           //else {
-             //console.log(addr + ' Not exist');
-           //}
+            if (mac) {
+              mqttID=mac;
+            }
+            else {
+              mqttID=Date.now();
+            }
+            client=s2006_mqtt.init(addr,mqttID, mqttMessageCb);
          }
 
          if (mac) {
@@ -115,7 +96,6 @@ var server = http.createServer(function(request, response) {
       case '/client/bye.mp3':
       case '/client/hello.mp3':
       case '/client/falling.mp3':
-    
       fs.readFile(__dirname +"/../"+ path, function(error, data) {
         if (error){
           response.writeHead(404);
@@ -146,14 +126,9 @@ io.attach(server,{
 console.log("Server set listen "+custom_port);
 server.listen(custom_port);
 console.log(addr);
-s2006_mqtt.init();
-
-io.on('connection',(socket) => {
-  //console.log(socket.address().address);
-  //console.log(socket.handshake.headers.host);
-  ftpHost="http://"+socket.handshake.headers.host;
-  console.log(ftpHost);
-  s2006_mqtt.client().on('message', function(topic, msg) {
+//s2006_mqtt.init(null, Date.now(),mqttMessageCb);
+let mqttDecryptData;
+function mqttMessageCb(msg) {
   data = AESCrypt.decrypt(msg);
     json=JSON.parse(data);
     json.time=util.getTime();
@@ -162,9 +137,9 @@ io.on('connection',(socket) => {
       if  (json.mac_address == filterMAC){
         sf.parser(json);
         console.log('======================= Get Sensor Data From MQTT '+filterMAC+'=========================' + json.time);
-        socket.emit('message', {'message': data});
+        socketID.emit('message', {'message': data});
+        mqttDecryptData=data;
         log.write(JSON.stringify(json),log.filename);
-        //log_to_file(JSON.stringify(json));
       }
       else {
         console.log('Device MAC:' +json.mac_address+ ' ( != )  ' +'Filter MAC'+  filterMAC+ '  == Data Length ==  ' + data.length);
@@ -173,6 +148,12 @@ io.on('connection',(socket) => {
     else {
       console.log("No define S2006 MAC==="+data );
     }
-    //socket.emit('message', {'message': data});
-  });
+  }
+io.on('connection',(socket) => {
+  //console.log(socket.address().address);
+  //console.log(socket.handshake.headers.host);
+  ftpHost="http://"+socket.handshake.headers.host;
+  console.log(ftpHost);
+  //socket.emit('message', {'message': mqttDecryptData});
+  socketID = socket;
 });
